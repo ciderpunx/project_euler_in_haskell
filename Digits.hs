feafcde1dc37cef0e24a0fb81002be8acf5de650
charlie <charlie@mallory.hendred.net>
2015-08-27 23:51:58 +0100

    Better Digits implementation

--- Digits.hs
+++ Digits.hs
@@ -4,5 +4,23 @@ import Data.Char
 digits :: Int -> [Int]
 digits = map digitToInt . show
 
+digitsLong :: Integer -> [Integer]
+digitsLong = map (toInteger . digitToInt) . show
+
+--- this is slightly slower for me
+digits' :: Int -> [Int]
+digits' = 
+  map (`mod` 10) . reverse . takeWhile (> 0) . iterate (`div` 10)
+
+undigitsLong :: [Integer] -> Integer
+undigitsLong = 
+    foldl addDigit 0
+   where 
+    addDigit num d = 10*num + d
+
 undigits :: [Int] -> Int
-undigits = read . map intToDigit
+undigits = fromIntegral . undigits
+
+-- slightly slower to do it this way (for me)
+undigits' :: [Int] -> Int
+undigits' = read . map intToDigit
